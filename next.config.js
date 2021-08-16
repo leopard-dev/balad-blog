63c3273164915fdb2b54b49ce7d15cc9ff9eec35
omid astaraki <omidocean@gmail.com>
2021-08-16 21:01:04 +0430

    chore: added css modules support to the project
    added @zeit/next-sass dependency and required configs to make css modules work

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const path = require("path");
 /** @type {import('next').NextConfig} */
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+module.exports = withSass({
+  cssModules: true,
+});
 module.exports = {
-  reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src", "styles")],
   },
