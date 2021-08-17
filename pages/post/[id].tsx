7f892cdc1e1f477876917c937842d36438e80561
omid astaraki <omidocean@gmail.com>
2021-08-17 12:15:35 +0430

    feat: changed rendering method to SSR

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -5,26 +5,13 @@ const BlogPost: NextPage = ({ post }: any) => {
   return <BlogPostListItem {...post} />;
 };
 
-export async function getStaticProps({ params }: any) {
+export async function getServerSideProps({ params }: any) {
   const post = await getPostById(params.id);
   return {
     props: {
       post,
     },
   };
-}
-
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const posts = await getAllPosts();
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: String(post.id) },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
 }
 
 export default BlogPost;
