c8de36241dd05fa81dced7e1b59ed3bd47e867a5
omid astaraki <omidocean@gmail.com>
2021-08-17 10:17:32 +0430

    fix: added promise rejection for status codes outside 2xx
    reject promises on server errors

--- src/services/post/index.ts
+++ src/services/post/index.ts
@@ -2,11 +2,19 @@ import { API_URL } from "../../constants";
 import { GetPostsResponse } from "./types";
 
 export const getAllPosts = async (): Promise<GetPostsResponse[]> => {
-  return (await fetch(`${API_URL}/posts`)).json();
+  const res = await fetch(`${API_URL}/posts`);
+  if (!res.ok) {
+    throw new Error("something went wrong");
+  }
+  return res.json();
 };
 
 export const getPostById = async (
   postId: number
 ): Promise<GetPostsResponse> => {
-  return (await fetch(`${API_URL}/posts/${postId}`)).json();
+  const res = await fetch(`${API_URL}/posts/${postId}`);
+  if (!res.ok) {
+    throw new Error("something went wrong");
+  }
+  return res.json();
 };
