1feef88da0143f0692a2ff11715566bbb2646dd0
omid astaraki <omidocean@gmail.com>
2021-08-17 13:56:40 +0430

    refactor: removed unnecessary pass Href

--- src/components/modules/SideBar/index.tsx
+++ src/components/modules/SideBar/index.tsx
@@ -7,17 +7,17 @@ function SideBar() {
       <h2 className={styles["app-sidebar__title"]}>لینک های بلاگ</h2>
       <ul className={styles["app-sidebar__links"]}>
         <li className={styles["app-sidebar__link-item"]}>
-          <Link href="/" passHref>
+          <Link href="/">
             <a className={styles["app-sidebar__link"]}>صفحه نخست</a>
           </Link>
         </li>
         <li className={styles["app-sidebar__link-item"]}>
-          <Link href="/" passHref>
+          <Link href="/">
             <a className={styles["app-sidebar__link"]}>درباره من</a>
           </Link>
         </li>
         <li className={styles["app-sidebar__link-item"]}>
-          <Link href="/" passHref>
+          <Link href="/">
             <a className={styles["app-sidebar__link"]}>تماس با من</a>
           </Link>
         </li>
