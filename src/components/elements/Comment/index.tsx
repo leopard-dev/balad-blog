bac4e5ffe2e08dfe7fa4a4c1c2db3c820cf00761
omid astaraki <omidocean@gmail.com>
2021-08-17 10:50:03 +0430

    refactor: transfered styles to css modules

--- src/components/elements/Comment/index.tsx
+++ src/components/elements/Comment/index.tsx
@@ -1,24 +1,28 @@
 import clsx from "clsx";
 import React from "react";
 import { GetPostComments } from "../../../services/post/types";
+import styles from "./styles.module.scss";
 
 type Props = { children?: React.ReactNode } & GetPostComments;
 
 function Comment({ author, body, date, id, children, parent_id }: Props) {
   return (
     <article
-      className={clsx("comment", { "comment--is-child": !!parent_id })}
+      className={clsx(
+        styles["comment"],
+        parent_id && styles["comment--is-child"]
+      )}
       id={`#c${id}`}
     >
       <link href={`#c${id}`} />
-      <footer className="comment__footer">
+      <footer className={styles["comment__footer"]}>
         <p className="comment__footer-element">
           ارسال شده توسط: <span>{author}</span>
         </p>
         <p className="comment__footer-element">{date}</p>
       </footer>
       <p className="comment__body">{body}</p>
-      <button className="comment__reply ">پاسخ به این نظر</button>
+      <button className="btn btn-secondary">پاسخ به این نظر</button>
       {children}
     </article>
   );
