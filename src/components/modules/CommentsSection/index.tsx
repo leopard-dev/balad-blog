a93a94495b2f0cdb1586cdfb5aafae0ee214340c
omid astaraki <omidocean@gmail.com>
2021-08-17 10:40:58 +0430

    feat: added on error retryability to comments section
    displays a button that retries the fetch request on demand when the initial request was failed

--- src/components/modules/CommentsSection/index.tsx
+++ src/components/modules/CommentsSection/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { getPostCommentsById } from "../../../services/post";
 import { GetPostComments } from "../../../services/post/types";
 import Comment from "../../elements/Comment";
@@ -11,13 +11,19 @@ function CommentsSection({ postId }: Props) {
   const [comments, setComments] = useState<GetPostComments[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  useEffect(() => {
-    setIsLoading(false);
-    getPostCommentsById(postId)
+
+  const fetchComments = useCallback((pId: number) => {
+    setIsLoading(true);
+    setIsError(false);
+    getPostCommentsById(pId)
       .then(setComments)
       .catch(() => setIsError(true))
       .finally(() => setIsLoading(false));
-  }, [postId]);
+  }, []);
+
+  useEffect(() => {
+    fetchComments(postId);
+  }, [fetchComments, postId]);
 
   const processedComments = useMemo(() => {
     const renderComment = (comment: GetPostComments) => {
@@ -39,7 +45,17 @@ function CommentsSection({ postId }: Props) {
       {isLoading && (
         <p className="comments-section__loading">لطفا صبر کنید ...</p>
       )}
-      {isError && <p className="comments-section__error">خطایی رخ داد ...</p>}
+      {isError && (
+        <p className="comments-section__error">
+          خطایی رخ داد ...{" "}
+          <button
+            className="btn btn-link"
+            onClick={() => fetchComments(postId)}
+          >
+            تلاش مجدد
+          </button>
+        </p>
+      )}
       {processedComments}
     </section>
   );
