82d90cd11b675bee21355b69c0d065a02f61fa98
omid astaraki <omidocean@gmail.com>
2021-08-18 18:56:33 +0430

    feat: added custom input field components
    custom InputField component with predefined styles and formik support

---
+++ src/components/elements/InputField/index.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { useField } from "formik";
+import styles from "./styles.module.scss";
+
+type Props = { label: string; [key: string]: any };
+
+function InputField({ label, ...props }: Props) {
+  const [field, meta, helpers] = useField(props as any);
+  return (
+    <div className={clsx("form-group", styles["input-field-container"])}>
+      <label htmlFor="exampleInputEmail1">{label}</label>
+      <input
+        className={clsx("form-control", {
+          "is-invalid": meta.touched && meta.error,
+        })}
+        {...field}
+        {...props}
+      />
+      <div className="invalid-feedback">{meta.error}</div>
+    </div>
+  );
+}
+
+export default InputField;
