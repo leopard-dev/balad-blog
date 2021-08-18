b3dfb21a4779996b4b783bf0314ffcd7ba780904
omid astaraki <omidocean@gmail.com>
2021-08-18 21:47:03 +0430

    feat: added retry option to homepage posts
    added internal state management for homepage
    failing serverside no longer sends 500 error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,40 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import BlogPostListItem from "../src/components/modules/BlogPostListItem";
 import { getAllPosts } from "../src/services/post";
 import { GetPostsResponse } from "../src/services/post/types";
 
 const Home: NextPage = ({ posts }: any) => {
+  const [isLoading, setIsLoading] = useState(posts.length === 0);
+  const [isError, setIsError] = useState(false);
+  const [internalPosts, setInternalPosts] = useState(posts);
+
+  const fetchPosts = () => {
+    setIsLoading(true);
+    setIsError(false);
+    getAllPosts()
+      .then(setInternalPosts)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (posts.length === 0) {
+      fetchPosts();
+    }
+  }, []);
   return (
     <>
-      {posts.map((post: GetPostsResponse) => (
+      {isLoading && <p>لطفا صبر کنید...</p>}
+      {isError && (
+        <p>
+          خطایی رخ داد
+          <button className="btn btn-link" onClick={fetchPosts}>
+            تلاش مجدد
+          </button>
+        </p>
+      )}
+      {internalPosts.map((post: GetPostsResponse) => (
         <BlogPostListItem key={post.id} {...post} />
       ))}
     </>
@@ -14,7 +42,10 @@ const Home: NextPage = ({ posts }: any) => {
 };
 
 export async function getServerSideProps() {
-  const posts = await getAllPosts();
+  let posts: GetPostsResponse[] = [];
+  try {
+    posts = await getAllPosts();
+  } catch {}
 
   return {
     props: {
