dfaf2593e88cd1d6a9ff2223847b3e1346e3ab91
omid astaraki <omidocean@gmail.com>
2021-08-18 21:51:58 +0430

    feat: added retry option for blogpost
    added internal state for blogpost
    added retryability to blogpost
    SSR errors no longer cause 500 response

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,14 +1,51 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import BlogPostListItem from "../../src/components/modules/BlogPostListItem";
-import { getAllPosts, getPostById } from "../../src/services/post";
-const BlogPost: NextPage = ({ post }: any) => {
-  return <BlogPostListItem {...post} />;
+import { getPostById } from "../../src/services/post";
+const BlogPost: NextPage = ({ post, postId }: any) => {
+  const [isLoading, setIsLoading] = useState(!post);
+  const [isError, setIsError] = useState(false);
+  const [internalPost, setInternalPost] = useState(post);
+
+  const fetchPost = () => {
+    setIsLoading(true);
+    setIsError(false);
+    getPostById(postId)
+      .then(setInternalPost)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (!post) {
+      fetchPost();
+    }
+  }, []);
+
+  return (
+    <>
+      {isLoading && <p>لطفا صبر کنید...</p>}
+      {isError && (
+        <p>
+          خطایی رخ داد
+          <button className="btn btn-link" onClick={fetchPost}>
+            تلاش مجدد
+          </button>
+        </p>
+      )}
+      {internalPost && <BlogPostListItem {...internalPost} />}
+    </>
+  );
 };
 
 export async function getServerSideProps({ params }: any) {
-  const post = await getPostById(params.id);
+  let post = null;
+  try {
+    post = await getPostById(params.id);
+  } catch {}
   return {
     props: {
+      postId: params.id,
       post,
     },
   };
