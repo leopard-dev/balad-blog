195e968ecbc4e25c4446707f649325e318e6782b
omid astaraki <omidocean@gmail.com>
2021-08-19 21:39:49 +0430

    feat: added inputType to InputField component
    added support for textarea inputs with auto resize

--- src/components/elements/InputField/index.tsx
+++ src/components/elements/InputField/index.tsx
@@ -2,20 +2,45 @@ import clsx from "clsx";
 import { useField } from "formik";
 import styles from "./styles.module.scss";
 
-type Props = { label: string; [key: string]: any };
+type Props = {
+  label: string;
+  inputType?: "textarea" | "input";
+  [key: string]: any;
+};
 
-function InputField({ label, validate, ...props }: Props) {
+function InputField({
+  label,
+  inputType: type = "input",
+  validate,
+  ...props
+}: Props) {
   const [field, meta, helpers] = useField({ ...props, validate } as any);
   return (
     <div className={clsx("form-group", styles["input-field-container"])}>
       <label htmlFor="exampleInputEmail1">{label}</label>
-      <input
-        className={clsx("form-control", {
-          "is-invalid": meta.touched && meta.error,
-        })}
-        {...field}
-        {...props}
-      />
+      {type === "input" && (
+        <input
+          className={clsx("form-control", {
+            "is-invalid": meta.touched && meta.error,
+          })}
+          {...field}
+          {...props}
+        />
+      )}
+      {type === "textarea" && (
+        <textarea
+          className={clsx("form-control", {
+            "is-invalid": meta.touched && meta.error,
+          })}
+          onKeyUp={(e: any) => {
+            e.target.style.overflow = "hidden";
+            e.target.style.height =
+              Math.max(e.target.height, e.target.scrollHeight) + "px";
+          }}
+          {...field}
+          {...props}
+        />
+      )}
       <div className="invalid-feedback">{meta.error}</div>
     </div>
   );
