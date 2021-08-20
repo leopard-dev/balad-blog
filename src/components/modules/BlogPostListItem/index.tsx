09874c70dea429a75cabe1ae4cadb690dd058ae8
omid astaraki <omidocean@gmail.com>
2021-08-20 13:01:22 +0430

    style: added required styles to break long words

--- src/components/modules/BlogPostListItem/index.tsx
+++ src/components/modules/BlogPostListItem/index.tsx
@@ -24,7 +24,7 @@ function BlogPostListItem({
       <p className={styles["blog-post__sub-title"]}>
         ارسال شده توسط {author} در تاریخ {date}
       </p>
-      <div className="blog-post__content">{parse(body)}</div>
+      <div className={styles["blog-post__content"]}>{parse(body)}</div>
     </article>
   );
 }
