be5c3bfa9fe68a8f070db54adb8bdcd2a219fc11
omid astaraki <omidocean@gmail.com>
2021-08-23 14:24:34 +0430

    feat: updated AutoResizableTextArea to use textarea element

--- src/components/elements/AutoResizableTextArea/index.tsx
+++ src/components/elements/AutoResizableTextArea/index.tsx
@@ -1,23 +1,58 @@
 import clsx from "clsx";
+import React, {
+  TextareaHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import styles from "./styles.module.scss";
 
 type Props = {
   error?: string;
-  [key: string]: any;
-};
+} & TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-function AutoResizableTextArea({ error, ...props }: Props) {
+const AutoResizableTextArea = ({ error, ...props }: Props) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [text, setText] = useState("");
+  const [textAreaHeight, setTextAreaHeight] = useState("auto");
+  const [parentHeight, setParentHeight] = useState("auto");
+
+  useEffect(() => {
+    setParentHeight(`${textAreaRef.current!.scrollHeight}px`);
+    setTextAreaHeight(`${textAreaRef.current!.scrollHeight}px`);
+  }, [text]);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextAreaHeight("auto");
+    setParentHeight(`${textAreaRef.current!.scrollHeight}px`);
+    setText(event.target.value);
+
+    if (props.onChange) {
+      props.onChange(event);
+    }
+  };
+
   return (
-    <span
-      role="textbox"
-      contentEditable
-      className={clsx("form-control", styles["textarea"], {
-        "is-invalid": error,
-      })}
-      {...props}
-    />
+    <div
+      style={{
+        minHeight: parentHeight,
+      }}
+    >
+      <textarea
+        className={clsx("form-control", styles["textarea"], {
+          "is-invalid": error,
+        })}
+        {...props}
+        ref={textAreaRef}
+        rows={1}
+        style={{
+          height: textAreaHeight,
+        }}
+        onChange={onChangeHandler}
+      />
+    </div>
   );
-}
+};
 
 export default AutoResizableTextArea;
